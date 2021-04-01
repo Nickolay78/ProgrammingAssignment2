dc58de253e913e8af1c0ba8345cd36374105933c
Nickolay78 <comcriminal@gmail.com>
2021-04-01 10:22:37 +0300

    Attempt to complete assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
-
+##Hello everyone. Have a nice day
+## Greetings from Severodonetsk, Ukraine
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() { x }
+  setinvers <- function( solve ) { m <<- solve }
+  getinvers <- function() { m }
+  list( set = set, get = get, setinvers = setinvers, getinvers = getinvers)
 }
-
 
 ## Write a short comment describing this function
 ## Hello Git
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinvers()
+  
+  if (!is.null(m)) {
+    message("retrieve the inverse from the cache")
+    return (m)
+  } 
+  
+  matX <- x$get()
+  ## Calculate the inverse of a matrix
+  m <- solve(matX, ...)
+  x$setinvers(m)
+  
+  m
+          ## Return a matrix that is the inverse of 'x'
 }
